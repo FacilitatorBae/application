95605c9f3b275bbd92ba1f62fecd2cb4228311cc
Julián Prieto <herbie.juls@gmail.com>
2023-06-23 20:27:43 -0300

    Refactor stuff (#4)

--- src/pages/search/[q].page.tsx
+++ src/pages/search/[q].page.tsx
@@ -2,31 +2,38 @@ import { CategoryList, ProductList } from "~/components";
 import { Select, Option } from "@material-tailwind/react";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
-import { menuItemsLabel, firstCharToCaps } from "./models";
+import {
+  sortMenuItems,
+  firstCharToCaps,
+  type SortField,
+  type SortCriteria,
+  type SortMenuItem,
+} from "./utils";
 import { useState } from "react";
 
 const Search = () => {
   const router = useRouter<"/search/[q]">();
-  const [sortField, setSortField] = useState("price");
-  const [sortCriteria, setSortCriteria] = useState("asc");
+  const [sort, setSort] = useState<{
+    field: SortField;
+    criteria: SortCriteria;
+  }>({ field: "price", criteria: "asc" });
 
   const { data: searchProducts } = api.products.getSearchProducts.useQuery(
     {
       text: router.query.q as string,
-      field: sortField as string,
-      criteria: sortCriteria as string,
+      field: sort.field,
+      criteria: sort.criteria,
     },
     { enabled: !!router.query.q }
   );
 
-  const onOptionClick = (item: string) => {
-    setSortField(menuItemsLabel[item]?.field);
-    setSortCriteria(menuItemsLabel[item].criteria);
+  const onOptionClick = (item: SortMenuItem) => {
+    setSort({ field: item.field, criteria: item.criteria });
   };
 
-  const sortOptions = Object.keys(menuItemsLabel).map((item) => (
-    <Option key={item} onClick={onOptionClick.bind(null, item)}>
-      {menuItemsLabel[item]?.label}
+  const sortOptions = sortMenuItems.map((item) => (
+    <Option key={item.label} onClick={() => onOptionClick(item)}>
+      {item.label}
     </Option>
   ));
 
