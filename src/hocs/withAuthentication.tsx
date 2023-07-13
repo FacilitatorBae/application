860db50f5202c7490b3884803bc5bd4da08c1dab
Julián Prieto <herbie.juls@gmail.com>
2023-07-12 21:17:23 -0300

    Add jsdoc

--- src/hocs/withAuthentication.tsx
+++ src/hocs/withAuthentication.tsx
@@ -1,6 +1,14 @@
 import { signIn, useSession } from "next-auth/react";
 
 // TODO: Figure out types... or not :)
+/**
+HOC to prevent a route from being rendered if the user is not logged in.
+
+withAuthentication args:
+@param Component Component/Route to render if the user is logged in.
+@param FallbackComponent Fallback Component/Route to render if the user is NOT logged in.
+In case it's not provided, the user will be redirected to the google login page.
+*/
 export const withAuthentication = (Component: any, FallbackComponent?: any) => {
   const AuthenticatedComponent = (props: any) => {
     const { status } = useSession();
